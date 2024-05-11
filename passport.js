7eb71f73146715e5c101834e44c721d5f54a8c29
Franklin Qu <qufrank0513@gmail.com>
2024-05-11 14:22:24 -0400

    updated passport.js

--- passport.js
+++ passport.js
@@ -18,10 +18,14 @@ passport.use(
       await Users.findOne({ username: username })
       .then((user) => {
         if (!user) {
-          console.log('incorrect username');
+          console.log('Incorrect username');
           return callback(null, false, {
             message: 'Incorrect username or password.',
           });
+        }
+        if (!user.validatePassword(password)) {
+            console.log('incorrect password');
+            return callback(null, false, { message: 'Incorrect password.' });
         }
         console.log('finished');
         return callback(null, user);
