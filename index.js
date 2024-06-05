d4bd3453b43f2de558b3c82667089bb2c6194fdb
Franklin Qu <qufrank0513@gmail.com>
2024-06-04 21:25:51 -0400

    commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 */
 
 // Connect to database remotely
-mongoose.connect(process.env.CONNECTION_URI, { 
+mongoose.connect(process.env.MONGO_URI, { 
     useNewUrlParser: true, 
     useUnifiedTopology: true 
 })
